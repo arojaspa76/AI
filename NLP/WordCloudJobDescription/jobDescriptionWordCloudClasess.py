06cef3bc6948513182ff78c94bb02764a6f01017
arojaspa76 <arojaspa@outlook.com>
2024-11-18 22:03:17 -0500

    delete manual stopword

--- NLP/WordCloudJobDescription/jobDescriptionWordCloudClasess.py
+++ NLP/WordCloudJobDescription/jobDescriptionWordCloudClasess.py
@@ -18,33 +18,6 @@ PLOT_INDEX = 30
 FILE_NAME_WITHOUT_EXT = ""
 DIRECTORY_NAME = ""
 
-'''
-STOPWORDS = set([
-    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because",
-    "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during",
-    "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's",
-    "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's",
-    "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our",
-    "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that",
-    "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're",
-    "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've",
-    "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "will", "with",
-    "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", "[UNK]"
-])
-
-STOPWORDS_ES = [
-    "a", "al", "algo", "alguien", "algunos", "ante", "antes", "como", "con", "contra", "cual", "cuales", "cualquier", 
-    "cuando", "cuanto", "cuantos", "de", "dejar", "del", "demasiado", "desde", "donde", "dos", "el", "ella", "ellas", 
-    "ellos", "empleado", "en", "entre", "era", "es", "esa", "esas", "ese", "esos", "estaba", "estaban", "estamos", 
-    "estar", "este", "esto", "estos", "estoy", "para", "para", "pero", "por", "porque", "quien", "quienes", "que", 
-    "quien", "que", "que", "sin", "sobre", "tal", "también", "tanto", "te", "toda", "todas", "todo", "todos", "tu", 
-    "tus", "un", "una", "unas", "uno", "unos", "usted", "vosotros", "vuestra", "y", "ya", "de", "y", "sólo", "tanta", 
-    "tantos", "se", "me", "mi", "mí", "nosotros", "nosotras", "nos", "vos", "vosotros", "ya", "mucha", "mucho", "muchos", 
-    "muchas", "cuando", "como", "a", "aquel", "aquella", "aquella", "alguien", "ahora", "algo", "[UNK]"
-]
-'''
-
-
 class jobDescriptionOperations:
     """
     Instantiates the jobDescriptionOperations class
